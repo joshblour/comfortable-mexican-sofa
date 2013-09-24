acd74c7c5f3e3399dfe0eff14b463bc13c017c23
joshblour <joshblour@hotmail.com>
2013-09-23 23:33:37 -0400

    Update application.js

--- app/assets/javascripts/comfortable_mexican_sofa/admin/application.js
+++ app/assets/javascripts/comfortable_mexican_sofa/admin/application.js
@@ -4,7 +4,7 @@ window.CMS.wysiwyg = function() {
   return tinymce.init({
     selector: "textarea",
     theme: "modern",
-    plugins: ["advlist autolink lists link image charmap print preview hr anchor pagebreak", "searchreplace wordcount visiualblocks visualchars code fullscreen", "insertdatetime media nonbreaking save table contextmenu directionality", "emoticons template paste textcolor"],
+    plugins: ["advlist autolink lists link image charmap print preview hr anchor pagebreak", "searchreplace wordcount visualchars code fullscreen", "insertdatetime media nonbreaking save table contextmenu directionality", "emoticons template paste textcolor"],
     toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
     toolbar2: "print preview media | forecolor backcolor emoticons",
     image_advtab: true
