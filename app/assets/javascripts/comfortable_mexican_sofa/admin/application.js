96c82c38226ca759920f4452c1598280f95edfc9
joshblour <joshblour@hotmail.com>
2013-09-23 23:15:30 -0400

    Update application.js

--- app/assets/javascripts/comfortable_mexican_sofa/admin/application.js
+++ app/assets/javascripts/comfortable_mexican_sofa/admin/application.js
@@ -1,1 +1,22 @@
-// Overwrite this file in your application /app/assets/javascripts/comfortable_mexican_sofa/admin/application.js 
+tinymce.init({
+    selector: "textarea",
+
+    theme: "modern",
+
+    plugins: [
+        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+
+        "searchreplace wordcount visualblocks visualchars code fullscreen",
+
+        "insertdatetime media nonbreaking save table contextmenu directionality",
+
+        "emoticons template paste textcolor"
+    ],
+
+    toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+
+    toolbar2: "print preview media | forecolor backcolor emoticons",
+
+    image_advtab: true,
+
+});
