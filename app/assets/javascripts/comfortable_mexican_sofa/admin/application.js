5ed1d0aa367f353906123f16da6f3f1293b004fa
joshblour <joshblour@hotmail.com>
2013-09-23 23:16:52 -0400

    Update application.js

--- app/assets/javascripts/comfortable_mexican_sofa/admin/application.js
+++ app/assets/javascripts/comfortable_mexican_sofa/admin/application.js
@@ -1,22 +1,10 @@
-tinymce.init({
+window.CMS.wysiwyg = function() {
+  return tinymce.init({
     selector: "textarea",
-
     theme: "modern",
-
-    plugins: [
-        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-
-        "searchreplace wordcount visualblocks visualchars code fullscreen",
-
-        "insertdatetime media nonbreaking save table contextmenu directionality",
-
-        "emoticons template paste textcolor"
-    ],
-
+    plugins: ["advlist autolink lists link image charmap print preview hr anchor pagebreak", "searchreplace wordcount visiualblocks visualchars code fullscreen", "insertdatetime media nonbreaking save table contextmenu directionality", "emoticons template paste textcolor"],
     toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-
     toolbar2: "print preview media | forecolor backcolor emoticons",
-
-    image_advtab: true,
-
-});
+    image_advtab: true
+  });
+};
